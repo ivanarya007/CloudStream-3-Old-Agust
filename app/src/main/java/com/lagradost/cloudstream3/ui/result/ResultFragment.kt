326321fd31658ac866a959ba836050d71e4a4b96
Stormunblessed <stormunblessed@gmail.com>
2022-03-29 10:19:44 -0600

    Fixes after merge

--- app/src/main/java/com/lagradost/cloudstream3/ui/result/ResultFragment.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/result/ResultFragment.kt
@@ -238,7 +238,9 @@ class ResultFragment : Fragment(), PanelsChildGestureRegionObserver.GestureRegio
                 TvType.Cartoon -> "Cartoons/$titleName"
                 TvType.Torrent -> "Torrent"
                 TvType.Documentary -> "Documentaries"
-                TvType.AsianDrama -> "AsianDrama"
+                TvType.Mirror -> "Mirror"
+                TvType.Donghua -> "Donghua"
+                TvType.AsianDrama -> "Asian Drama"
             }
 
             val src = "$DOWNLOAD_NAVIGATE_TO/$parentId" // url ?: return@let
@@ -1243,7 +1245,12 @@ class ResultFragment : Fragment(), PanelsChildGestureRegionObserver.GestureRegio
         }
 
         observe(viewModel.dubStatus) { status ->
-            result_dub_select?.text = status.toString()
+            val dubstatusName = if (status.name == "Subbed") getString(R.string.dub_status_subbed)
+            else if (status.name == "Dubbed") getString(R.string.dub_status_dubbed)
+            else if (status.name == "PremiumDub") getString(R.string.dub_status_premium)
+            else if (status.name == "PremiumSub") getString(R.string.sub_status_premium)
+            else ""
+            result_dub_select?.text = dubstatusName
         }
 
         val preferDub = context?.getApiDubstatusSettings()?.all { it == DubStatus.Dubbed } == true
@@ -1275,9 +1282,14 @@ class ResultFragment : Fragment(), PanelsChildGestureRegionObserver.GestureRegio
             if (ranges != null) {
                 it.popupMenuNoIconsAndNoStringRes(ranges
                     .map { status ->
+                        val dubstatusName = if (status.name == "Subbed") getString(R.string.dub_status_subbed)
+                        else if (status.name == "Dubbed") getString(R.string.dub_status_dubbed)
+                        else if (status.name == "PremiumDub") getString(R.string.dub_status_premium)
+                        else if (status.name == "PremiumSub") getString(R.string.sub_status_premium)
+                        else ""
                         Pair(
                             status.ordinal,
-                            status.toString()
+                            dubstatusName
                         )
                     }
                     .toList()) {
@@ -1673,7 +1685,10 @@ class ResultFragment : Fragment(), PanelsChildGestureRegionObserver.GestureRegio
                                 TvType.Documentary -> R.string.documentaries_singular
                                 TvType.Movie -> R.string.movies_singular
                                 TvType.Torrent -> R.string.torrent_singular
-                                TvType.AsianDrama -> R.string.asian_drama_singular
+                                TvType.Mirror -> R.string.mirror_singular
+                                TvType.Donghua -> R.string.donghua_singular
+                                TvType.AsianDrama -> R.string.asian_dramas_singular
+
                             }
                         )?.let {
                             result_meta_type?.text = it
